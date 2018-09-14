3395148ea266e40a92f8dd85fcb3e27b995695bb
TK <tk@TKs-MacBook-Pro.local>
2018-09-14 12:18:48 -0700

    remove the geth directory before create the genesis block

--- build/deploy/init_mine.sh
+++ build/deploy/init_mine.sh
@@ -15,4 +15,5 @@ fi
 ACCOUNT=`cat /tmp/account.js | cut -d "{" -f2 | cut -d "}" -f1`
 cp genesis.json /tmp/newgenesis.json
 sed -i "s/abc8a1591aa743ecfb4c0556aa91105e9edd1718/${ACCOUNT}/" /tmp/newgenesis.json
+rm -rf $MINER_DATADIR/geth
 ./geth --datadir $MINER_DATADIR init /tmp/newgenesis.json
