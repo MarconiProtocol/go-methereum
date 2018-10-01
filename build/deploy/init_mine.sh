4acdcf91ba808460f46886ed5494ad1502448df6
TK <tk@TKs-MacBook-Pro.local>
2018-10-01 14:23:45 -0700

    using fixed userid

--- build/deploy/init_mine.sh
+++ build/deploy/init_mine.sh
@@ -14,9 +14,4 @@ HOME=`eval echo "~$USER"`
 MINER_DATADIR=$HOME/$MINER_DATADIR
 
 rm -rf $MINER_DATADIR
-./geth --datadir $MINER_DATADIR account new --password <(echo "neji") > /tmp/account.js
-ACCOUNT=`cat /tmp/account.js | cut -d "{" -f2 | cut -d "}" -f1`
-cp genesis.json /tmp/newgenesis.json
-sed -i "s/abc8a1591aa743ecfb4c0556aa91105e9edd1718/${ACCOUNT}/" /tmp/newgenesis.json
-rm -rf $MINER_DATADIR/geth
-./geth --datadir $MINER_DATADIR init /tmp/newgenesis.json
+./geth --datadir $MINER_DATADIR init genesis.json
