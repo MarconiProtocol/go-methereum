8048aeb85bd06a85e5bde0f948e32f00b2482b86
albert <a@neji.io>
2018-12-13 01:16:35 +0000

    set default ports to 30304 and set bootnode port to 30304 (use geth instance as bootnode test)

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -502,7 +502,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 30303,
+		Value: 30304,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
