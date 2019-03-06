00b096dbe2542aa6035f39867a5df46784f31fe1
Darryl <d@neji.io>
2019-03-06 13:46:40 -0800

    Touch ups to get everything compiling again.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -926,38 +926,6 @@ func MakePasswordList(ctx *cli.Context) []string {
 	return lines
 }
 
-
-type allocItem struct{ Addr, Balance *big.Int }
-
-type allocList []allocItem
-
-func (a allocList) Len() int           { return len(a) }
-func (a allocList) Less(i, j int) bool { return a[i].Addr.Cmp(a[j].Addr) < 0 }
-func (a allocList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-
-func makelist(g *core.Genesis) allocList {
-	a := make(allocList, 0, len(g.Alloc))
-	for addr, account := range g.Alloc {
-		if len(account.Storage) > 0 || len(account.Code) > 0 || account.Nonce != 0 {
-			panic(fmt.Sprintf("can't encode account %x", addr))
-		}
-		a = append(a, allocItem{addr.Big(), account.Balance})
-	}
-	sort.Sort(a)
-	return a
-}
-
-func makealloc(g *core.Genesis) string {
-	a := makelist(g)
-	data, err := rlp.EncodeToBytes(a)
-	if err != nil {
-		panic(err)
-	}
-	return strconv.QuoteToASCII(string(data))
-}
-
-
-
 func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 	setNodeKey(ctx, cfg)
 	setNAT(ctx, cfg)
