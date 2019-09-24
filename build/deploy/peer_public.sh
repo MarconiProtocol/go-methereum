47742e2ba1df76e2a8284d71f67254d34a4771b6
Di Wu <di@marconi.org>
2019-09-24 15:57:01 -0700

    make peer.sh do light sync by default

--- build/deploy/peer_public.sh
+++ build/deploy/peer_public.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /opt/marconi/etc/meth/datadir
 
-./gmeth --networkid 161027 --port 13200 --rpc --rpcapi=db,eth,net,web3,personal --rpcport 10004 --datadir /opt/marconi/etc/meth/datadir/
+./gmeth --networkid 161027 --port 13200 --rpc --rpcapi=db,eth,net,web3,personal --rpcport 10004 --datadir /opt/marconi/etc/meth/datadir/ --syncmode "light"
